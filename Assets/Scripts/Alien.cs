a341417e4dbfbe53f1e9c96d0315864460d0d648
ClareChan1738 <claregpbenn@icloud.com>
2023-09-24 17:15:35 -0400

    Chapter 5 - Added Alien Collisions & Despawn Code.

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -8,6 +8,8 @@ public class Alien : MonoBehaviour
 
     public Transform target;
     private NavMeshAgent agent;
+    public float navigationUpdate;
+    private float navigationTime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,17 @@ public class Alien : MonoBehaviour
     {
         if (target != null)
         {
-            agent.destination = target.position;
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                agent.destination = target.position;
+                navigationTime = 0;
+            }
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
