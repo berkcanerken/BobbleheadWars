72e1aaa0c8bacca6b0e7d8840feb5d4f34a0a8cf
ClareChan1738 <claregpbenn@icloud.com>
2023-09-24 14:46:57 -0400

    Chapter 4 - Added Player Bobblehead Physics & Added Colliders.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
+    public Rigidbody head;
 
     // Start is called before the first frame update
     void Start()
@@ -22,4 +23,19 @@ public class PlayerController : MonoBehaviour
         characterController.SimpleMove(moveDirection * moveSpeed);
 
     }
+
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
+    }
+
 }
+
