b1e3480c920df2f77d6c7b313ddf833f4c2ceb7b
ClareChan1738 <claregpbenn@icloud.com>
2023-10-06 15:04:05 -0400

    Chapter 6 - Added Raising & Lowering Walls Animation

---
+++ Assets/Scripts/ArenaWall.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaWall : MonoBehaviour
+{
+    private Animator arenaAnimator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject arena = transform.parent.gameObject;
+        arenaAnimator = arena.GetComponent<Animator>();
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        arenaAnimator.SetBool("IsLowered", true);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        arenaAnimator.SetBool("IsLowered", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
